3abedacbe126bff850463b34cade1480fde70a30
Atharva Rai <atharvarai07@gmail.com>
2026-06-18 13:38:59 +0530

    doc analyzer high level artitechture

---
+++ src/doc_analyzer/data_analysis.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_classic.output_parsers import OutputFixingParser
+from utils.models_loader import ModelLoader
+from models.models import *
+
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import CustomException
+
+logger = CustomLogger().get_logger(__file__)
+
+class DocumentAnalyzer:
+    """
+    Analyzes documents using a pre-trained language model.
+    Automatically logs all the action and supports section-based organization.
+    """
+
+    def __init__(self):
+        pass
+
+    def analyze_metadata(self):
+        pass
---
+++ src/doc_analyzer/data_ingestion.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import fitz
+import uuid
+from datetime import datetime
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import CustomException
+
+logger = CustomLogger().get_logger(__file__)
+
+class DocumentHandler:
+    """
+    Handles PDF saving and reading operations.
+    Automatically logs all the action and supports section-based organization.
+    """
+
+    def __init__(self):
+        pass
+
+    def save_pdf(self):
+        pass
+
+    def read_pdf(self):
+        pass
+
+
+
