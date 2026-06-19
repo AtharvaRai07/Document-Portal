2a3b4ae081158228341b284a40f18f866b80b764
Atharva Rai <atharvarai07@gmail.com>
2026-06-19 13:21:48 +0530

    Completed doc analyzer whole code

--- src/doc_analyzer/data_analysis.py
+++ src/doc_analyzer/data_analysis.py
@@ -3,6 +3,7 @@ import sys
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_classic.output_parsers import OutputFixingParser
 from utils.models_loader import ModelLoader
+from prompts.prompt_library import *
 from models.models import *
 
 from logger.custom_logger import CustomLogger
@@ -17,7 +18,37 @@ class DocumentAnalyzer:
     """
 
     def __init__(self):
-        pass
+        try:
+            self.loader = ModelLoader()
+            self.llm = self.loader.load_llm_model()
 
-    def analyze_document(self):
-        pass
+            self.parser = JsonOutputParser(pydantic_object=MetaData)
+            self.fixing_parser = OutputFixingParser.from_llm(self.llm, self.parser)
+
+            self.prompt = document_analysis_prompt
+
+            logger.info("DocumentAnalyzer initialized successfully with LLM and parsers.")
+
+        except Exception as e:
+            logger.error(f"Error initializing DocumentAnalyzer: {e}")
+            raise CustomException(f"Error initializing DocumentAnalyzer: {e}", sys)
+
+    def analyze_document(self, document_text: str) -> dict:
+        """
+        Analyzes the document using the pre-trained language model and returns structured metadata.
+        """
+        try:
+            chain = self.prompt | self.llm | self.fixing_parser
+            logger.info("Metadata analysis chain initialized successfully.")
+
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
+            })
+
+            logger.info("Metadata extraction completed successfully.", keys=list(response.keys()))
+            return response
+
+        except Exception as e:
+            logger.error(f"Error analyzing document: {e}")
+            raise CustomException(f"Error analyzing document: {e}", sys)
